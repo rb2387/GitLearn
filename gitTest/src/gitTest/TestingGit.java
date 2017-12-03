b9167418a42c980d1d6736560d0be98876019490
Rohit Baweja <rbaweja2387@gmail.com>
2017-12-03 13:09:28 +0530

    Initial Checkin of Git Learning

---
+++ gitTest/src/gitTest/TestingGit.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class TestingGit {
+
+	public static void main(String[] args) {
+		System.out.println("we are moving to git");
+		System.out.println(Calendar.THURSDAY);
+		System.out.println(Calendar.DECEMBER);
+	}
+	
+}
